dcaaf10a8260275b43537f9e93180a8db988eb02
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-11-12 14:23:07 +0530

    Add files via upload

---
+++ Python program to reverse a string.py
@@ -0,0 +1,13 @@
+#### 12th Nov
+
+### https://www.w3resource.com/python-exercises/python-functions-exercises.php -- 4
+
+# Sample String : "1234abcd"
+# Expected Output : "dcba4321"
+
+s = '1234abcd'
+name = 'Indrani Sarkar'
+
+print(s[::-1])   ### dcba4321
+print('\n')
+print(name[::-1])   #### rakraS inardnI
