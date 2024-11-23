2305681e1e26b8d5581c41fe5ab94e96ff2baef9
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-11-23 17:34:14 +0530

    Add files via upload

---
+++ Python whether a number falls in a given range.py
@@ -0,0 +1,11 @@
+########  23rd Nov
+
+def range_check(n, range_start, range_end):
+    n = int(input("Enter the n "))
+    range_start = int(input("Enter the starting "))
+    range_end = int(input("Enter the ending "))
+
+    while n <= (range_start+range_end)/2:
+        print(i)
+
+        5           ---- 8 -- 10
