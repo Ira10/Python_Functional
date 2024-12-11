2e4b92ea06b8de700b153cd0b51760cd2cf77dc6
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-12-11 21:42:04 +0530

    Add files via upload

---
+++ Accept a hyphen-separated sequence of words as input and prints the sorted words.py
@@ -0,0 +1,9 @@
+########   11th Dec 
+
+##### https://www.w3resource.com/python-exercises/python-functions-exercise-15.php
+
+str_ = 'green-red-black-white'
+
+items = str_.split('-')
+
+print('-'.join(sorted(items)))  ### black-green-red-white
